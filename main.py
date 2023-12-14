45bc42ebf6d5dc6efc2cd6bea4d5c373ab3e425f
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2023-12-15 02:14:00 +0300

    Added basic api crud routes

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ def index():
     return {
         "message":"Welcome to my first apis"
     }
+    
+@app.get('/courses')
+def courses():
+    return []    
+    
+@app.post('/courses')
+def create():
+    return {
+        "message":"Course created successfully"
+    }
+    
+@app.patch('/courses/{course_id}')
+def update_course(course_id:int):
+    return {"message":f"Course {course_id} updated successfully"}
+
+@app.delete('/courses/{course_id}')
+def delete_course(course_id:int):
+    return {"message":f"Course {course_id} deleted successfully"}
